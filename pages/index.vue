5a3f56273b99e4dfa672b1bfb469dd563d0041fe
yassh <yassh32@outlook.jp>
2018-09-12 21:22:49 +0900

    update index.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Figure skating protocol generator 2017-2018</h1>
+    <div><input v-model="foo" /></div>
+    <div>{{foo}}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      foo: "bar"
+    }
+  }
+}
+</script>
